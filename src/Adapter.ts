7f667638c690573c25e23e777450c3025888e0ff
Guille Díaz <alu0101505688@ull.edu.es>
2024-03-06 10:32:15 +0000

    modified

--- src/Adapter.ts
+++ src/Adapter.ts
@@ -1,7 +1,8 @@
-// Hacer que la clase complex entienda los racionales.
 import { Complex } from "./Complex";
 import { Rational } from "./Rational";
-
+/**
+ * Class consisting on an adapter to operate with complex and rational numbers mixed.
+ */
 export class Adapter extends Complex {
   constructor(private rational: Rational){
     super(rational.num1 / rational.num2, 0);
